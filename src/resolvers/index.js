c6a35b58a8385a6e6444668b86c05c4fcbfe5c5b
Venancio Souza <venanciodesouza@icloud.com>
2020-04-25 11:14:49 -0300

    add led pwm

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,13 +1,13 @@
 const { UserInputError, PubSub, withFilter } = require("apollo-server");
 const Gpio = require("pigpio").Gpio;
+const dht = require("node-dht-sensor");
 
 const resolvers = {
   Query: {
     DHT: async (root, args) => {
       const { pin } = await args;
-      // const led = new Gpio(pin, { mode: Gpio.OUTPUT });
-      // led.digitalWrite(state);
-      // console.log(`Actived led on GPIO ${pin}`);
+      const res = await dht.read(11, parseInt(pin));
+      console.log(res);
       return { temperature: 100.0, humidity: 100.0, pin: pin };
     },
   },
