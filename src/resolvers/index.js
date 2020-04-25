8a90d013fb64187c6230dd9d2ace54d4cd8a22ee
Venancio Souza <venanciodesouza@icloud.com>
2020-04-25 11:20:24 -0300

    add led pwm

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -6,7 +6,7 @@ const resolvers = {
   Query: {
     DHT: async (root, args) => {
       const { pin } = await args;
-      const res = await dht.read(11, parseInt(pin));
+      const res = await dht.read(11, pin);
       console.log(res);
       return { temperature: 100.0, humidity: 100.0, pin: pin };
     },
