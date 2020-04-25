b275d888a3d7af09d4ee05ff96f2a1d97bd1520d
Venancio Souza <venanciodesouza@icloud.com>
2020-04-24 22:12:58 -0300

    add led

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -4,10 +4,10 @@ const Gpio = require("pigpio").Gpio;
 const resolvers = {
   Mutation: {
     turnOnOff: async (root, args) => {
-      // const { state, pin } = await args;
-      // const led = new Gpio(pin, { mode: Gpio.OUTPUT });
-      // led.digitalWrite(state);
-      // console.log(`Actived led on GPIO ${pin}`);
+      const { state, pin } = await args;
+      const led = new Gpio(pin, { mode: Gpio.OUTPUT });
+      led.digitalWrite(state);
+      console.log(`Actived led on GPIO ${pin}`);
       return args;
     },
   },
