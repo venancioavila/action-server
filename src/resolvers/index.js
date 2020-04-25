65b4cef94c13ab40d11888c6f18acee042dc2791
Venancio Souza <venanciodesouza@icloud.com>
2020-04-25 11:03:03 -0300

    add led pwm

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -5,9 +5,9 @@ const resolvers = {
   Query: {
     DHT: async (root, args) => {
       const { pin } = await args;
-      const led = new Gpio(pin, { mode: Gpio.OUTPUT });
-      led.digitalWrite(state);
-      console.log(`Actived led on GPIO ${pin}`);
+      // const led = new Gpio(pin, { mode: Gpio.OUTPUT });
+      // led.digitalWrite(state);
+      // console.log(`Actived led on GPIO ${pin}`);
       return { temperature: 100.0, humidity: 100.0, pin: pin };
     },
   },
