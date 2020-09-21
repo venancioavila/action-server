a313da76edd2287f4843d5dac4164856c4e9c46d
Venancio Souza <venanciodesouza@icloud.com>
2020-09-21 18:42:37 -0300

    (fix): remove conmments

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ const resolvers = require("./resolvers");
 const IP = require("ip");
 require("dotenv").config();
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
-// The `listen` method launches a web server.
+
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`⚡  Server running...`);
   console.log(`➡️  ${IP.address()} `);
